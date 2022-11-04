e0e461cf4c7c995befbf23bb0acc67f3ec510620
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-11-04 10:23:20 +0530

    Create 70.ClimbingStairs.cpp

---
+++ Easy_Level/70.ClimbingStairs.cpp
@@ -0,0 +1,33 @@
+/*Leetcode link: https://leetcode.com/problems/climbing-stairs/description/
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+        return n;
+  
+     
+
+//fibonacci approarch
+
+   int x = 1;
+        int y = 2;
+		for(int i = 3; i <= n; i++)
+		{
+            int sum = x + y;
+            x = y;
+            y = sum;
+        }
+        return y;
+    }
+};

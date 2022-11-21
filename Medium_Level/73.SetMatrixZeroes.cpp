0b210e15dcb1d720bcbc46bd1f6af4309f16b911
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-11-21 18:26:29 +0530

    Update 73.SetMatrixZeroes.cpp

--- Medium_Level/73.SetMatrixZeroes.cpp
+++ Medium_Level/73.SetMatrixZeroes.cpp
@@ -1,5 +1,5 @@
 /*
-
+Leetcode link: https://leetcode.com/problems/set-matrix-zeroes/description/
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 You must do it in place.
 Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]

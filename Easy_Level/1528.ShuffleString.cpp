3bcc3a61765d8d2d1539a502b262b0ff66d10467
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-11-29 01:39:15 +0530

    Update 1528.ShuffleString.cpp

--- Easy_Level/1528.ShuffleString.cpp
+++ Easy_Level/1528.ShuffleString.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/shuffle-string/description/
 You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 Return the shuffled string.
 Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]

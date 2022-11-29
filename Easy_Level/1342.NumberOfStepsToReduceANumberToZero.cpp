9670adf514054f1fb80f2b3501e3642fc41f2c3b
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-11-30 00:07:17 +0530

    Update 1342.NumberOfStepsToReduceANumberToZero.cpp

--- Easy_Level/1342.NumberOfStepsToReduceANumberToZero.cpp
+++ Easy_Level/1342.NumberOfStepsToReduceANumberToZero.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
 Given an integer num, return the number of steps to reduce it to zero.
 In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 Input: num = 14

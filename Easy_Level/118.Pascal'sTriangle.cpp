34ba9a5afe7249cd44dc7e6473bcf94ab320fa55
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-04 00:04:52 +0530

    Update 118.Pascal'sTriangle.cpp

--- Easy_Level/118.Pascal'sTriangle.cpp
+++ Easy_Level/118.Pascal'sTriangle.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/pascals-triangle/
 Given an integer numRows, return the first numRows of Pascal's triangle.
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 Input: numRows = 5

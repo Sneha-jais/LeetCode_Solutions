abbfeba0a7e8bb2e7f7fe8bfa9c2475d4f333e2b
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-03 19:14:58 +0530

    Update 1486.XOROperationInAnArray.cpp

--- Easy_Level/1486.XOROperationInAnArray.cpp
+++ Easy_Level/1486.XOROperationInAnArray.cpp
@@ -2,6 +2,10 @@
 You are given an integer n and an integer start.
 Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
 Return the bitwise XOR of all elements of nums.
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
 */
 
 class Solution {

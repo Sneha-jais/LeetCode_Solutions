cd4f0dd29993b7b9ab5e60e2bd459ad9da4eda69
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-05 07:07:36 +0530

    Update 121.BestTimeToBuyAndSellStock.cpp

--- Easy_Level/121.BestTimeToBuyAndSellStock.cpp
+++ Easy_Level/121.BestTimeToBuyAndSellStock.cpp
@@ -4,6 +4,7 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 Input: prices = [7,1,5,3,6,4]
 Output: 5
+
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 */
@@ -22,6 +23,5 @@ public:
         }
         return maximum;
     }
-    
 };
 

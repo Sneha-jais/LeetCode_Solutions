b1d647194cfd837394e3dd0e620712e72302d57f
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-17 01:12:29 +0530

    Update 58.LengthOfLastWord.cpp

--- Easy_Level/58.LengthOfLastWord.cpp
+++ Easy_Level/58.LengthOfLastWord.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/length-of-last-word/description/
 Given a string s consisting of words and spaces, return the length of the last word in the string.
 A word is a maximal 
 substring

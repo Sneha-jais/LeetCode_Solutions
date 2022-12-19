7c61ea00ab79f72958250437d46b7ac2d59e28d6
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-20 00:09:26 +0530

    Update 168. ExcelSheetColumnTitle.cpp

--- Easy_Level/168. ExcelSheetColumnTitle.cpp
+++ Easy_Level/168. ExcelSheetColumnTitle.cpp
@@ -1,3 +1,22 @@
+/*Leetcode link: https://leetcode.com/problems/excel-sheet-column-title/description/
+
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+
+Input: columnNumber = 28
+Output: "AB"
+*/
+
+
 class Solution {
 public:
     string convertToTitle(int cn) {

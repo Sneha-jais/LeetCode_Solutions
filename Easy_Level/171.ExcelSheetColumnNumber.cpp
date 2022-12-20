3bfa33b793900cdd7fa144899820e2951e3f7466
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-21 00:44:51 +0530

    Update 171.ExcelSheetColumnNumber.cpp

--- Easy_Level/171.ExcelSheetColumnNumber.cpp
+++ Easy_Level/171.ExcelSheetColumnNumber.cpp
@@ -1,4 +1,21 @@
+/*
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
 
+Input: columnTitle = "AB"
+Output: 28
+
+Input: columnTitle = "ZY"
+Output: 701
+*/
 
 class Solution {
 public:

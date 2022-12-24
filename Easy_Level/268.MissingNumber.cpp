5f34dd09bd0e0a794c3d56d52fddafaa2419f6a7
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-24 19:44:18 +0530

    Update 268.MissingNumber.cpp

--- Easy_Level/268.MissingNumber.cpp
+++ Easy_Level/268.MissingNumber.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link: 
+/* Leetcode link:  https://leetcode.com/problems/missing-number/description/
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 Input: nums = [3,0,1]
 Output: 2

44018559d18952603cce3ac09b85dafc74dda60d
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-25 17:24:29 +0530

    Create 263.UglyNumber.cpp

---
+++ Easy_Level/263.UglyNumber.cpp
@@ -0,0 +1,30 @@
+/* Leetcode link:
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+Given an integer n, return true if n is an ugly number.
+Example 1:
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+
+*/
+
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==1)
+        return true;
+          if(n==0)
+        return false;
+      
+        if (n % 2 == 0)
+        return isUgly(n / 2);
+    if (n % 3 == 0)
+        return isUgly(n / 3);
+    if (n % 5 == 0)
+        return isUgly(n / 5);
+ 
+  
+    return false;
+    }
+};

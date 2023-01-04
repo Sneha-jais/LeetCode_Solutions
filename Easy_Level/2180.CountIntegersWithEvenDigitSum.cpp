ac14bc78e60c376b5de7d13bf008325c2f198ae7
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-04 13:29:53 +0530

    Update 2180.CountIntegersWithEvenDigitSum.cpp

--- Easy_Level/2180.CountIntegersWithEvenDigitSum.cpp
+++ Easy_Level/2180.CountIntegersWithEvenDigitSum.cpp
@@ -1,4 +1,12 @@
-
+/* Leetcode link:
+Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+The digit sum of a positive integer is the sum of all its digits.
+Example 1:
+Input: num = 4
+Output: 2
+Explanation:
+The only integers less than or equal to 4 whose digit sums are even are 2 and 4.    
+*/
 
 class Solution {
 public:

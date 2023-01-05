18b77e6fdd57ecb4b801b9c3efd9cc3de76a77e9
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-05 14:34:43 +0530

    Update 1952.ThreeDivisors.cpp

--- Easy_Level/1952.ThreeDivisors.cpp
+++ Easy_Level/1952.ThreeDivisors.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link: 
+/* Leetcode link: https://leetcode.com/problems/three-divisors/description/
 Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
 An integer m is a divisor of n if there exists an integer k such that n = k * m.
 Example 1:

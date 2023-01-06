29a464bf0488b6bcb78ebc57f58b6ee6f8f73678
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-06 17:25:41 +0530

    Create 1974.MinimumTimetoTypeWordUsingSpecialTypewriter.cpp

---
+++ Easy_Level/1974.MinimumTimetoTypeWordUsingSpecialTypewriter.cpp
@@ -0,0 +1,13 @@
+/*
+There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. 
+A character can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
+Input: word = "abc"
+Output: 5
+Explanation: 
+The characters are printed as follows:
+- Type the character 'a' in 1 second since the pointer is initially on 'a'.
+- Move the pointer clockwise to 'b' in 1 second.
+- Type the character 'b' in 1 second.
+- Move the pointer clockwise to 'c' in 1 second.
+- Type the character 'c' in 1 second.
+*/

c42c6bb7fe19cf8584813f42aa92f802a83bcf22
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-06 17:26:30 +0530

    Update 1974.MinimumTimetoTypeWordUsingSpecialTypewriter.cpp

--- Easy_Level/1974.MinimumTimetoTypeWordUsingSpecialTypewriter.cpp
+++ Easy_Level/1974.MinimumTimetoTypeWordUsingSpecialTypewriter.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/description/
 There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. 
 A character can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
 Input: word = "abc"
@@ -11,3 +11,17 @@ The characters are printed as follows:
 - Move the pointer clockwise to 'c' in 1 second.
 - Type the character 'c' in 1 second.
 */
+
+class Solution {
+public:
+    int minTimeToType(string w) {
+        int n=w.size();
+        int ans = 0;
+        char temp = 'a';
+        for (int i=0;i<n;i++) {
+           ans += min(abs(w[i] - temp), 26 - abs(w[i] - temp)) + 1;
+            temp = w[i];
+        }
+        return ans;
+    }
+};

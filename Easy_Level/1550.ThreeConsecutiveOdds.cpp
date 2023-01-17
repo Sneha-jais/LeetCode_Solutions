98f30dc49672f84b9f9d3434eebb8194eefd8048
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-17 13:57:51 +0530

    Update 1550.ThreeConsecutiveOdds.cpp

--- Easy_Level/1550.ThreeConsecutiveOdds.cpp
+++ Easy_Level/1550.ThreeConsecutiveOdds.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/three-consecutive-odds/
 Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
 Example 1:
 Input: arr = [2,6,4,1]

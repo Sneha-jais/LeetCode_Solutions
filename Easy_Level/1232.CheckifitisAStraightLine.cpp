88dfc50918efb8640f4b02191445c6c096f794bf
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-20 14:39:00 +0530

    Update and rename 1232.CheckIfItIsAStraightLine.cpp to 1232.CheckifitisAStraightLine.cpp

--- Easy_Level/1232.CheckifitisAStraightLine.cpp
+++ Easy_Level/1232.CheckifitisAStraightLine.cpp
@@ -1,5 +1,12 @@
+/*
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. 
+Check if these points make a straight line in the XY plane.
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
 
-
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+*/
 
 class Solution {
 public:

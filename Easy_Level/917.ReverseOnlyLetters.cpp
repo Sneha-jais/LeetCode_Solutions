96331392a6c615b65e2f66b06b4a681df2830494
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-28 14:31:59 +0530

    Update 917.ReverseOnlyLetters.cpp

--- Easy_Level/917.ReverseOnlyLetters.cpp
+++ Easy_Level/917.ReverseOnlyLetters.cpp
@@ -1,4 +1,12 @@
-
+/*
+Given a string s, reverse the string according to the following rules:
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+Example 1:
+Input: s = "ab-cd"
+Output: "dc-ba"
+*/
 
 class Solution {
 public:

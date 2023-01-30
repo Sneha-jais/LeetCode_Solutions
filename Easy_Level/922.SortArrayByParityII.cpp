b96f01d19dafd14e54390ac1581c1b9bae23fb18
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-30 23:16:57 +0530

    Update 922.SortArrayByParityII.cpp

--- Easy_Level/922.SortArrayByParityII.cpp
+++ Easy_Level/922.SortArrayByParityII.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/sort-array-by-parity-ii/description/
 Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
 Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
 Return any answer array that satisfies this condition.

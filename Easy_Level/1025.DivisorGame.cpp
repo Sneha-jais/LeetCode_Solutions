d5792876541de4faf82b65e69c57b841562a58bf
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-31 22:28:23 +0530

    Create 1025.DivisorGame.cpp

---
+++ Easy_Level/1025.DivisorGame.cpp
@@ -0,0 +1,13 @@
+
+
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+       bool flag=false;
+        if(n%2==0){
+            flag=true;
+        }        
+        return flag;
+    }
+};

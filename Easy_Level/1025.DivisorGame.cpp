afa77e7dac55342d5f711a6c6bb3bf0d47d80ff7
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-01-31 22:30:19 +0530

    Update 1025.DivisorGame.cpp

--- Easy_Level/1025.DivisorGame.cpp
+++ Easy_Level/1025.DivisorGame.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/divisor-game/description/
 Alice and Bob take turns playing a game, with Alice starting first.
 Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
 Choosing any x with 0 < x < n and n % x == 0.

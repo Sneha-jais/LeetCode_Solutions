25bfa9fe84b584cec037a142f8e20e31f859a64d
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-04 13:40:02 +0530

    Update 876.MiddleofTheLinkedList.cpp

--- Easy_Level/876.MiddleofTheLinkedList.cpp
+++ Easy_Level/876.MiddleofTheLinkedList.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/middle-of-the-linked-list/
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
 Input: head = [1,2,3,4,5]

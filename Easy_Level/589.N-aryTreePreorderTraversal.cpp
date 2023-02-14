706a843fdd5b79a0254064c7a17f50eac74787f2
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-14 20:36:42 +0530

    Update 589.N-aryTreePreorderTraversal.cpp

--- Easy_Level/589.N-aryTreePreorderTraversal.cpp
+++ Easy_Level/589.N-aryTreePreorderTraversal.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
 Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 Input: root = [1,null,3,2,4,null,5,6]

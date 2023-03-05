2ada80db353a5bd12b7ba9a0789d6703d9eccc3f
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-05 23:20:29 +0530

    Update 1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp

--- Easy_Level/1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
+++ Easy_Level/1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
@@ -2,6 +2,7 @@
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
  
  Input: n = 234
+ 
 Output: 15 
 Explanation: 
 Product of digits = 2 * 3 * 4 = 24 

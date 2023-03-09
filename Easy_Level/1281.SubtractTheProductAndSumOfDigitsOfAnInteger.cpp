6b848f98833b40ecd793db03190fd975f22aa3d8
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-09 23:14:24 +0530

    Update 1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp

--- Easy_Level/1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
+++ Easy_Level/1281.SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
@@ -1,6 +1,6 @@
 /* Leetcode link: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
- 
+
  Input: n = 234
  
 Output: 15 

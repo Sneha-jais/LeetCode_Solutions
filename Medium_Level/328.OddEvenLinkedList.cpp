1baf9a2b685124c95e7b4312529be4c9a78d2a4e
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-16 21:55:15 +0530

    Update 328.OddEvenLinkedList.cpp

--- Medium_Level/328.OddEvenLinkedList.cpp
+++ Medium_Level/328.OddEvenLinkedList.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/odd-even-linked-list/
 Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 The first node is considered odd, and the second node is even, and so on.
 Note that the relative order inside both the even and odd groups should remain as it was in the input.

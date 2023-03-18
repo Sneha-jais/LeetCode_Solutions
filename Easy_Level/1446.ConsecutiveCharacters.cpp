25cfc13689f7113104e71ce7dfe396c42b835d36
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-18 23:52:56 +0530

    Update 1446.ConsecutiveCharacters.cpp

--- Easy_Level/1446.ConsecutiveCharacters.cpp
+++ Easy_Level/1446.ConsecutiveCharacters.cpp
@@ -1,4 +1,11 @@
-
+/* Leetcode link: https://leetcode.com/problems/consecutive-characters/
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+Given a string s, return the power of s.
+Example 1:
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+*/
 
 class Solution {
 public:

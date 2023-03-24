8cb0d07aadb9c89b78d575c6d5b8189f7811559d
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-24 20:03:34 +0530

    Update 28.FindtheIndexoftheFirstOccurrenceinaString.cpp

--- Easy_Level/28.FindtheIndexoftheFirstOccurrenceinaString.cpp
+++ Easy_Level/28.FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 Input: haystack = "sadbutsad", needle = "sad"
 Output: 0

3538c58525a083cd6b5d53c10b9fa65b55cd0615
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-03-27 23:03:17 +0530

    Update 102.BinaryTreeLevelOrderTraversal.cpp

--- Medium_Level/102.BinaryTreeLevelOrderTraversal.cpp
+++ Medium_Level/102.BinaryTreeLevelOrderTraversal.cpp
@@ -5,3 +5,33 @@ Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[9,20],[15,7]]
 
 */
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+      if (root == nullptr)
+      return {};
+
+    vector<vector<int>> ans;
+    queue<TreeNode*> q{{root}};
+
+    while (!q.empty()) {
+      vector<int>temp;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode* node = q.front();
+        q.pop();
+        temp.push_back(node->val);
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+      
+      ans.push_back(temp);
+    }
+
+    return ans;
+
+    }
+};

042d721ca362d4913530828c5a379019f59e37e7
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-04-04 23:10:11 +0530

    Update 958.CheckCompletenessofaBinaryTree.cpp

--- Medium_Level/958.CheckCompletenessofaBinaryTree.cpp
+++ Medium_Level/958.CheckCompletenessofaBinaryTree.cpp
@@ -1,4 +1,4 @@
-/*
+/* Leetcode link: https://leetcode.com/problems/check-completeness-of-a-binary-tree/
 Given the root of a binary tree, determine if it is a complete binary tree.
 In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible.
 It can have between 1 and 2h nodes inclusive at the last level h.

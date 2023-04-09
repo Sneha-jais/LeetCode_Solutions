613f63cfd04063a707d200cc0efa763bdff51756
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-04-09 23:02:39 +0530

    Update 896.MonotonicArray.cpp

--- Easy_Level/896.MonotonicArray.cpp
+++ Easy_Level/896.MonotonicArray.cpp
@@ -1,4 +1,4 @@
-/* Leetcode link:
+/* Leetcode link: https://leetcode.com/problems/monotonic-array/
 An array is monotonic if it is either monotone increasing or monotone decreasing.
 An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
 Given an integer array nums, return true if the given array is monotonic, or false otherwise.

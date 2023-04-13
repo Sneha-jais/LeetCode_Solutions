261fc2b70222af68e62f03408c60cf63f8a90de6
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-04-13 23:50:43 +0530

    Update 1431.KidsWiththeGreatestNumberofCandies.cpp

--- Easy_Level/1431.KidsWiththeGreatestNumberofCandies.cpp
+++ Easy_Level/1431.KidsWiththeGreatestNumberofCandies.cpp
@@ -5,7 +5,8 @@ Return a boolean array result of length n, where result[i] is true if, after giv
 they will have the greatest number of candies among all the kids, or false otherwise.
 Note that multiple kids can have the greatest number of candies.
 
- Example 1:
+
+ Example:
 Input: candies = [2,3,5,1,3], extraCandies = 3
 Output: [true,true,true,false,true] 
 Explanation: If you give all extraCandies to:
@@ -14,6 +15,7 @@ Explanation: If you give all extraCandies to:
 - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
 - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
 - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
 */
 
 class Solution {

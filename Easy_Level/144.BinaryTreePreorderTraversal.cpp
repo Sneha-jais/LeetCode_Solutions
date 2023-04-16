b1bdd81fa9082a7b5a39cc4c9f7d9ddbda29d29c
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-04-16 22:03:54 +0530

    Update 144.BinaryTreePreorderTraversal.cpp

--- Easy_Level/144.BinaryTreePreorderTraversal.cpp
+++ Easy_Level/144.BinaryTreePreorderTraversal.cpp
@@ -1,4 +1,11 @@
+/* Leetcode link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Input: root = [1,null,2,3]
+Output: [1,2,3]
 
+Input: root = []
+Output: []
+*/
 
 
 /**

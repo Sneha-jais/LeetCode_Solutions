3fbdf8b54305b3daac0566649f20bf25b06eb4d3
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-05-27 23:15:19 +0530

    Update 100.SameTree.cpp

--- Easy_Level/100.SameTree.cpp
+++ Easy_Level/100.SameTree.cpp
@@ -1,7 +1,6 @@
 /* Leetcode link: https://leetcode.com/problems/same-tree/
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.
 Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-
 Input: p = [1,2,3], q = [1,2,3]
 Output: true
 Input: p = [1,2], q = [1,null,2]
